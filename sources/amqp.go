d24f6ad4e2dd2c2ccafe16c1942b39e2dc44a2c1
Alexander Baryshnikov <dev@baryshnikov.net>
2018-08-31 15:13:12 +0800

    add amqp handler with retry

--- sources/amqp.go
+++ sources/amqp.go
@@ -18,3 +18,17 @@ func MakeAMQPHandler(vm *machine.StateMachine) fluent.TransactionHandlerFunc {
 		return vm.Emit(context.WithValue(ctx, AMQPMessageKey, msg), id, msg.Body)
 	}
 }
+
+func MakeAMQPHandlerWithRetry(vm *machine.StateMachine, retryQueue fluent.Requeue) fluent.TransactionHandlerFunc {
+	return func(ctx context.Context, msg amqp.Delivery) error {
+		id := msg.CorrelationId
+		if id == "" {
+			id = msg.MessageId
+		}
+		err := vm.Emit(context.WithValue(ctx, AMQPMessageKey, msg), id, msg.Body)
+		if err != nil {
+			return retryQueue.Requeue(&msg)
+		}
+		return nil
+	}
+}
